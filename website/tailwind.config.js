1d40fb87cf2c7d8837a1b86441af1f04e0f3e5b1
Adam Jones <domdomegg+git@gmail.com>
2021-09-20 01:40:02 +0200

    Fix tailwind intellisense

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,33 +1,33 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false,
   theme: {
     extend: {
       colors: {
-        'raise-purple': '#5D215E',
-        'raise-red': '#AD2E53',
-        'raise-blue': '#2ECAD6',
-        'raise-orange': '#E88452',
-        'raise-yellow': '#F2CA1A',
+        "raise-purple": "#5D215E",
+        "raise-red": "#AD2E53",
+        "raise-blue": "#2ECAD6",
+        "raise-orange": "#E88452",
+        "raise-yellow": "#F2CA1A",
       },
       fontFamily: {
-        'raise-header': ['laca', ...defaultTheme.fontFamily.sans],
-        'raise-content': ['mr-eaves-sans', ...defaultTheme.fontFamily.sans],
+        "raise-header": ["laca", ...defaultTheme.fontFamily.sans],
+        "raise-content": ["mr-eaves-sans", ...defaultTheme.fontFamily.sans],
       },
       skew: {
-        '15': '15deg',
-        '-15': '-15deg',
+        15: "15deg",
+        "-15": "-15deg",
       },
       boxShadow: {
-        'raise': '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
-      }
+        raise: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
+      },
     },
   },
   variants: {
     extend: {
-      translate: ['group-hover']
+      translate: ["group-hover"],
     },
   },
   plugins: [],
