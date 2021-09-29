2c44bdb37ae887d2ac923188a091bf5499efa811
Adam Jones <domdomegg+git@gmail.com>
2021-09-29 13:02:00 +0000

    Update philosophy section

--- website/src/components/Philosophy.tsx
+++ website/src/components/Philosophy.tsx
@@ -1,31 +1,35 @@
 import * as React from "react"
-import { ArrowRightIcon } from "@heroicons/react/outline"
-
-import moneyBox from "../images/moneyBox.svg"
-import party from "../images/party.svg"
+import {
+  StarIcon, CakeIcon, AcademicCapIcon, CurrencyPoundIcon,
+} from "@heroicons/react/outline"
+import classNames from "classnames"
 
 const Philosophy: React.FC<{}> = () => (
   <div className="max-w-2xl mx-auto text-left">
-    <div className="flex">
-      <img alt="" src={moneyBox} width={60} />
-      <p className="ml-8">
-        We believe that choosing to give deliberately, rather than engaging with charity passively or out of obligation, can be a positive and rewarding experience. As such, we invite students to make a personally significant donation as a way of celebrating the end of the academic year.
-      </p>
-    </div>
-    <div className="flex mt-6">
-      <img alt="" style={{ filter: "invert(1)" }} src={party} width={60} />
-      <p className="ml-8">
-        We aim to show that by putting giving at the heart of celebration, we can have a huge impact and a wonderful time in the process. Our Summer Party embodies this philosophy, as students come together to celebrate the collective impact of their donations.
-      </p>
-    </div>
-    <div className="flex mt-6">
-      <div><ArrowRightIcon width={60} /></div>
-      <p className="ml-8">
-        We encourage students to take this positive, meaningful approach to giving forwards into their future lives, ultimately creating a culture whereby deliberate, effective giving is normalised at university and beyond.
-      </p>
-    </div>
+    <Point className="bg-raise-purple" icon={StarIcon}>
+      At Raise, we believe in the power of giving positively and deliberately, rather than engaging with charity passively or out of obligation. In our view, giving in this way is more meaningful, more sustainable, and ultimately more impactful.
+    </Point>
+    <Point className="bg-raise-red" icon={CurrencyPoundIcon}>
+      That’s why, once a year, we invite students to make a donation that feels personally significant - an amount that makes you think about the reasons for your donation. Ultimately, we want giving to become a considered and meaningful action.
+    </Point>
+    <Point className="bg-raise-orange" icon={CakeIcon}>
+      Our Summer Party embodies this attitude towards our giving. It’s a time when we come together to celebrate our collective impact, while enjoying the end of the year with a wonderful community.
+    </Point>
+    <Point className="bg-raise-yellow text-black" icon={AcademicCapIcon}>
+      We want to create a culture where deliberate, positive giving is normalised at university and beyond. And so, we encourage students to take this philosophy forward into their future lives, for example by pursuing an impactful career or taking a long-term giving pledge.
+    </Point>
+
     <iframe className="w-full mt-12" title="YouTube: Rethinking Celebration: The Positive Case for Giving" width="560" height="315" src="https://www.youtube-nocookie.com/embed/fvjeG7xE-wE" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
     <p className="text-center text-xl mt-2">Check out our founder's TEDx Talk, 'Rethinking Celebration: The Positive Case for Giving', which talks all about our feel-good philosophy!</p>
+  </div>
+)
+
+const Point: React.FC<{ className?: string, icon: React.FC<{ width?: number, height?: number }> }> = ({ className, icon: Icon, children }) => (
+  <div className={classNames("flex mt-4 p-4 rounded", className)}>
+    <div><Icon width={60} height={60} /></div>
+    <p className="ml-8">
+      {children}
+    </p>
   </div>
 )
 
