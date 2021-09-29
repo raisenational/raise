a46a2637609e5b2697c3973f8d96a0b69f71e1ab
Adam Jones <domdomegg+git@gmail.com>
2021-09-29 14:12:48 +0200

    Add philosophy and contact section to national page

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -2,10 +2,12 @@ import * as React from "react"
 import Helmet from "react-helmet"
 
 import Page from "../components/Page"
-import Section from "../components/Section"
+import Section, { SectionTitle } from "../components/Section"
 import Navigation from "../components/Navigation"
-import Cover from "../components/Cover"
+import Cover, { CTADown } from "../components/Cover"
 import IntroStats from "../components/IntroStats"
+import Philosophy from "../components/Philosophy"
+import ContactForm from "../components/ContactForm"
 
 const IndexPage = () => (
   <Page>
@@ -13,7 +15,7 @@ const IndexPage = () => (
       <title>Raise</title>
       <meta property="og:title" content="Raise" />
     </Helmet>
-    <Cover className="-mb-16">
+    <Cover>
       <Navigation
         left={[
           { text: "Cambridge", href: "https://www.mayweekalternative.org.uk/" },
@@ -24,7 +26,7 @@ const IndexPage = () => (
         right={[
         ]}
       />
-      <Section className="m-auto px-8 pb-8">
+      <Section className="px-8">
         <IntroStats
           title="Raise: A Celebration of Giving"
           tagline="Raise is a charitable movement encouraging students to adopt a positive approach towards deliberate, effective giving. Check out our four chapter websites to learn more about what we do."
@@ -36,7 +38,21 @@ const IndexPage = () => (
           }}
         />
       </Section>
+      <CTADown
+        text="Our philosophy"
+        href="#our-philosophy"
+      />
     </Cover>
+
+    <Section id="our-philosophy">
+      <SectionTitle>Our Philosophy</SectionTitle>
+      <Philosophy />
+    </Section>
+
+    <Section id="contact">
+      <SectionTitle>Get in Touch</SectionTitle>
+      <ContactForm className="mt-8" action="https://formspree.io/f/mnqlrnvq" />
+    </Section>
   </Page>
 )
 
