e431b20c0b521cb7fdde10f9a1827ed7c5204bd4
Adam Jones <jones_adam@rocketmail.com>
2021-10-22 22:39:16 +0100

    Simplify DonationCard formatting

--- website/src/components/DonationCard.tsx
+++ website/src/components/DonationCard.tsx
@@ -8,7 +8,7 @@ type Props = PublicFundraiser["donations"][number] & {
   className?: string,
 }
 
-// TODO: this is a bit of a mess, but should be fairly simple. Maybe refactor to make the logic a bit clearer, or at least add some unit tests around it.
+// TODO: refactor to make the logic clearer, and add some unit tests around it
 const DonationCard: React.FC<Props> = ({
   donorName,
   createdAt,
@@ -20,10 +20,10 @@ const DonationCard: React.FC<Props> = ({
   className,
 }) => (
   <div className={classNames("p-4 rounded flex flex-col shadow-raise", className)}>
-    <p className={classNames({ "flex-1": !comment && !giftAid && !matchFundingAmount })}>{donorName ?? "Someone"} donated {donationAmount && amountDropPenceIfZeroFormatter(donationAmount)}</p>
-    {(giftAid || matchFundingAmount) && <p className={classNames("text-base opacity-80", { "flex-1": !comment })}> ({matchFundingAmount && `+${amountDropPenceIfZeroFormatter(matchFundingAmount)} matched`}{giftAid && matchFundingAmount && ", "}{giftAid && donationAmount && `+${amountDropPenceIfZeroFormatter(donationAmount * 0.25)} gift-aided`})</p>}
-    {comment && <p className="text-base flex-1 mt-2">{comment}</p>}
-    <p className="text-base opacity-80 mt-2 text-right"><TimeAgo date={createdAt * 1000} /></p>
+    <p>{donorName ?? "Someone"} donated {donationAmount && amountDropPenceIfZeroFormatter(donationAmount)}</p>
+    {(giftAid || matchFundingAmount) && <p className="text-base opacity-80"> ({matchFundingAmount && `+${amountDropPenceIfZeroFormatter(matchFundingAmount)} matched`}{giftAid && matchFundingAmount && ", "}{giftAid && donationAmount && `+${amountDropPenceIfZeroFormatter(donationAmount * 0.25)} gift-aided`})</p>}
+    {comment && <p className="text-base mt-2">{comment}</p>}
+    <p className="text-base opacity-80 mt-auto pt-2 text-right"><TimeAgo date={createdAt * 1000} /></p>
   </div>
 )
 
