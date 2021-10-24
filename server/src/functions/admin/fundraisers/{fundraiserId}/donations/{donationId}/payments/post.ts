a7f5779787235238fe47b28305a89a609447a3b8
Adam Jones <jones_adam@rocketmail.com>
2021-10-24 20:32:39 +0100

    Update manual donation endpoint

--- server/src/functions/admin/fundraisers/{fundraiserId}/donations/{donationId}/payments/post.ts
+++ server/src/functions/admin/fundraisers/{fundraiserId}/donations/{donationId}/payments/post.ts
@@ -2,24 +2,29 @@ import "source-map-support/register"
 import { ulid } from "ulid"
 import { middyfy } from "../../../../../../../helpers/wrapper"
 import {
-  appendList, assertHasGroup, get,
+  assertHasGroup, get, insert,
 } from "../../../../../../../helpers/db"
 import { paymentEditsSchema, ulidSchema } from "../../../../../../../helpers/schemas"
-import { donationTable, fundraiserTable } from "../../../../../../../helpers/tables"
+import { fundraiserTable, donationTable, paymentTable } from "../../../../../../../helpers/tables"
 
 export const main = middyfy(paymentEditsSchema, ulidSchema, true, async (event) => {
   assertHasGroup(event, await get(fundraiserTable, { id: event.pathParameters.fundraiserId }))
   const paymentId = ulid()
   const { fundraiserId, donationId } = event.pathParameters
 
-  appendList(donationTable, { fundraiserId, id: donationId }, "payments", {
+  insert(paymentTable, {
     id: paymentId,
+    donationId,
     at: event.body.at ?? Math.floor(new Date().getTime() / 1000),
     amount: event.body.amount ?? 0,
-    method: event.body.method,
-    reference: event.body.reference,
-    status: event.body.status,
+    method: event.body.method ?? "cash",
+    reference: event.body.reference ?? null,
+    status: event.body.status ?? "paid",
   })
+
+  // TODO: update donation amount
+  // TODO: update fundraiser amount
+  // TODO: do all three tasks in a transaction
 
   return paymentId
 })
