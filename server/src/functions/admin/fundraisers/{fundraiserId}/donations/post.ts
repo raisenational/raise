53fe70265b74394cf0d932678f291785c9ce24e5
Malena Schmidt <malena-schmidt2000@web.de>
2021-10-25 18:36:49 +0000

    Transactions

--- server/src/functions/admin/fundraisers/{fundraiserId}/donations/post.ts
+++ server/src/functions/admin/fundraisers/{fundraiserId}/donations/post.ts
@@ -2,7 +2,9 @@ import "source-map-support/register"
 import createHttpError from "http-errors"
 import { ulid } from "ulid"
 import { middyfy } from "../../../../../helpers/wrapper"
-import { assertHasGroup, get, insert } from "../../../../../helpers/db"
+import {
+  assertHasGroup, get, insertT, inTransaction, plusT,
+} from "../../../../../helpers/db"
 import { donationEditsSchema, ulidSchema } from "../../../../../helpers/schemas"
 import { donationTable, fundraiserTable } from "../../../../../helpers/tables"
 
@@ -13,36 +15,46 @@ export const main = middyfy(donationEditsSchema, ulidSchema, true, async (event)
     throw new createHttpError.BadRequest(`Fundraiser id in body (${event.body.fundraiserId}) does not match path parameter (${event.pathParameters.fundraiserId})`)
   }
 
+  const donationId = ulid()
   // TODO: validate match funding amount against limit? or not given this is a manual entry?
   // TODO: validate gift-aid requirements
   // TODO: add amount to fundraiser totalRaised, subtract amount from fundraiser matchFundingRemaining
   // TODO: do this in a transaction
 
-  const donation = await insert(donationTable, {
-    id: ulid(),
-    fundraiserId: event.pathParameters.fundraiserId,
-    donorName: event.body.donorName ?? "Unknown",
-    donorEmail: event.body.donorEmail ?? "Unknown",
-    createdAt: event.body.createdAt ?? Math.floor(new Date().getTime() / 1000),
-    addressLine1: event.body.addressLine1 ?? null,
-    addressLine2: event.body.addressLine2 ?? null,
-    addressLine3: event.body.addressLine3 ?? null,
-    addressPostcode: event.body.addressPostcode ?? null,
-    addressCountry: event.body.addressCountry ?? null,
-    giftAid: event.body.giftAid ?? false,
-    comment: event.body.comment ?? null,
-    donationAmount: event.body.donationAmount ?? 0,
-    matchFundingAmount: event.body.matchFundingAmount ?? 0,
-    contributionAmount: event.body.contributionAmount ?? 0,
-    recurringAmount: event.body.recurringAmount ?? null,
-    recurrenceFrequency: event.body.recurrenceFrequency ?? null,
-    stripeId: event.body.stripeId ?? "",
-    payments: [],
-    charity: event.body.charity ?? "AMF",
-    overallPublic: event.body.overallPublic ?? false,
-    namePublic: event.body.namePublic ?? false,
-    donationAmountPublic: event.body.donationAmountPublic ?? false,
-  })
+  inTransaction([
+    insertT(
+      donationTable,
+      {
+        id: donationId,
+        fundraiserId: event.pathParameters.fundraiserId,
+        donorName: event.body.donorName ?? "Unknown",
+        donorEmail: event.body.donorEmail ?? "Unknown",
+        createdAt: event.body.createdAt ?? Math.floor(new Date().getTime() / 1000),
+        addressLine1: event.body.addressLine1 ?? null,
+        addressLine2: event.body.addressLine2 ?? null,
+        addressLine3: event.body.addressLine3 ?? null,
+        addressPostcode: event.body.addressPostcode ?? null,
+        addressCountry: event.body.addressCountry ?? null,
+        giftAid: event.body.giftAid ?? false,
+        comment: event.body.comment ?? null,
+        donationAmount: event.body.donationAmount ?? 0,
+        matchFundingAmount: event.body.matchFundingAmount ?? 0,
+        contributionAmount: event.body.contributionAmount ?? 0,
+        recurringAmount: event.body.recurringAmount ?? null,
+        recurrenceFrequency: event.body.recurrenceFrequency ?? null,
+        stripeId: event.body.stripeId ?? "",
+        charity: event.body.charity ?? "AMF",
+        overallPublic: event.body.overallPublic ?? false,
+        namePublic: event.body.namePublic ?? false,
+        donationAmountPublic: event.body.donationAmountPublic ?? false,
+      },
+    ),
+    plusT(
+      fundraiserTable,
+      { id: event.pathParameters.fundraiserId },
+      { donationsCount: 1 },
+    ),
+  ])
 
-  return donation.id
+  return donationId
 })
--- server/src/functions/admin/fundraisers/{fundraiserId}/donations/{donationId}/payments/post.ts
+++ server/src/functions/admin/fundraisers/{fundraiserId}/donations/{donationId}/payments/post.ts
@@ -2,7 +2,7 @@ import "source-map-support/register"
 import { ulid } from "ulid"
 import { middyfy } from "../../../../../../../helpers/wrapper"
 import {
-  assertHasGroup, get, insert,
+  assertHasGroup, get, insertT, inTransaction, plusT, insert,
 } from "../../../../../../../helpers/db"
 import { paymentEditsSchema, ulidSchema } from "../../../../../../../helpers/schemas"
 import { fundraiserTable, donationTable, paymentTable } from "../../../../../../../helpers/tables"
@@ -11,20 +11,47 @@ export const main = middyfy(paymentEditsSchema, ulidSchema, true, async (event) 
   assertHasGroup(event, await get(fundraiserTable, { id: event.pathParameters.fundraiserId }))
   const paymentId = ulid()
   const { fundraiserId, donationId } = event.pathParameters
+  const paymentAmount = event.body.amount ?? 0
 
-  insert(paymentTable, {
-    id: paymentId,
-    donationId,
-    at: event.body.at ?? Math.floor(new Date().getTime() / 1000),
-    amount: event.body.amount ?? 0,
-    method: event.body.method ?? "cash",
-    reference: event.body.reference ?? null,
-    status: event.body.status ?? "paid",
-  })
+  if (event.body.status !== "paid") {
+    insert(paymentTable, {
+      id: paymentId,
+      donationId,
+      at: event.body.at ?? Math.floor(new Date().getTime() / 1000),
+      amount: paymentAmount,
+      method: event.body.method ?? "cash",
+      reference: event.body.reference ?? null,
+      status: event.body.status ?? "paid",
+    })
+  }
 
-  // TODO: update donation amount
-  // TODO: update fundraiser amount
-  // TODO: do all three tasks in a transaction
+  const [fundraiser, donation] = await Promise.all([
+    get(fundraiserTable, { id: fundraiserId }),
+    get(donationTable, { fundraiserId, id: donationId }),
+  ])
+
+  const matchFundingAdded = Math.max(Math.min(Math.floor(paymentAmount * (fundraiser.matchFundingRate / 100)), fundraiser.matchFundingRemaining ?? Infinity, (fundraiser.matchFundingPerDonationLimit ?? Infinity) - donation.matchFundingAmount), 0)
+
+  inTransaction([
+    insertT(paymentTable, {
+      id: paymentId,
+      donationId,
+      at: event.body.at ?? Math.floor(new Date().getTime() / 1000),
+      amount: paymentAmount,
+      method: event.body.method ?? "cash",
+      reference: event.body.reference ?? null,
+      status: event.body.status ?? "paid",
+    }),
+    // we need to check the matchfundingamount on this donation has not increased since we got the data
+    // so that we do not go over the limit on matchfunding per donation
+    plusT(donationTable, { fundraiserId: event.pathParameters.fundraiserId, id: donationId }, { donationAmount: paymentAmount, matchFundingAmount: matchFundingAdded }, "matchFundingAmount = :currentMatchFundingAmount", { ":currentMatchFundingAmount": donation.matchFundingAmount }),
+    // we differentiate between matchFundingRemaining === null which is when there is infinite matchfunding
+    // if there is infinite matchfunding we need to check that is still the case when we try to add the matchfundingamount
+    // if there is limited matchfunding we need to check that there is still enough matchfunding left for this payment (and this hasn't changed since we got the data from the database)
+    fundraiser.matchFundingRemaining === null
+      ? plusT(fundraiserTable, { id: event.pathParameters.fundraiserId }, { totalRaised: paymentAmount + matchFundingAdded }, "matchFundingRemaining = :null", { ":null": null })
+      : plusT(fundraiserTable, { id: event.pathParameters.fundraiserId }, { totalRaised: paymentAmount + matchFundingAdded, matchFundingRemaining: -matchFundingAdded }, "matchFundingRemaining >= :matchFundingAdded", { ":matchFundingAdded": matchFundingAdded }),
+  ])
 
   return paymentId
 })
