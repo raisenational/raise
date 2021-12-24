abda3f8e95fcd1e7d845951cd8c16bdc75ab93e5
Adam Jones <jones_adam@rocketmail.com>
2021-12-24 12:36:18 +0000

    Upgrade Gatsby from v2 to v4

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
       },
     },
     "gatsby-plugin-typescript",
+    "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-react-helmet",
