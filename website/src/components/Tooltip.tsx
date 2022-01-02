ec845bb08b4458af5f343d890f6643dbe63502ad
Adam Jones <domdomegg+git@gmail.com>
2022-01-02 14:08:50 +0000

    Donations flow improvements for mobiles (#22)

---
+++ website/src/components/Tooltip.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const Tooltip: React.FC<{
+  label: React.ReactNode,
+  children: React.ReactNode,
+}> = ({ label, children }) => (
+  <span className="group underline underline-offset-1 decoration-dotted cursor-help">
+    {children}
+    <span className="tooltip">{label}</span>
+  </span>
+)
+
+export default Tooltip
