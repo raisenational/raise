db21e395e56475f709bf5c4af64c05bb516ebee7
Adam Jones <domdomegg+git@gmail.com>
2022-01-08 18:22:51 +0000

    Restrict editing fundraiser currency and editing payments

--- server/src/api/admin/fundraisers/{fundraiserId}/patch.ts
+++ server/src/api/admin/fundraisers/{fundraiserId}/patch.ts
@@ -1,14 +1,22 @@
 import { fundraiserEditsSchema } from "@raise/shared"
+import createHttpError from "http-errors"
 import { middyfy } from "../../../../helpers/wrapper"
 import {
-  assertHasGroup, assertHasGroupForProperties, checkPrevious, get, update,
+  assertHasGroup, assertHasGroupForProperties, checkPrevious, get, query, update,
 } from "../../../../helpers/db"
-import { fundraiserTable } from "../../../../helpers/tables"
+import { donationTable, fundraiserTable } from "../../../../helpers/tables"
 import { NATIONAL } from "../../../../helpers/groups"
 
 export const main = middyfy(fundraiserEditsSchema, null, true, async (event) => {
   assertHasGroup(event, await get(fundraiserTable, { id: event.pathParameters.fundraiserId }))
-  assertHasGroupForProperties(event, NATIONAL, ["totalRaised", "donationsCount", "matchFundingRate", "matchFundingPerDonationLimit", "matchFundingRemaining", "groupsWithAccess"])
+  assertHasGroupForProperties(event, NATIONAL, ["currency", "totalRaised", "donationsCount", "matchFundingRate", "matchFundingPerDonationLimit", "matchFundingRemaining", "groupsWithAccess"])
+
+  if (event.body.currency) {
+    const donations = await query(donationTable, { fundraiserId: event.pathParameters.fundraiserId })
+    if (donations.length > 0) {
+      throw new createHttpError.BadRequest("Cannot edit currency on fundraiser with donations")
+    }
+  }
 
   await update(fundraiserTable, { id: event.pathParameters.fundraiserId }, ...checkPrevious(event.body))
 })
