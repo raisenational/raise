829355faa2ea58fee182fd41c5a9a03b0e8798ad
Adam Jones <domdomegg+git@gmail.com>
2022-01-08 17:29:56 +0000

    Fix canonical urls

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,19 +1,23 @@
+require('ts-node').register()
+
+const env = require('./src/env/env').default
+
 module.exports = {
   siteMetadata: {
     title: "Raise",
     description: "A charitable movement encouraging students to adopt a positive approach towards deliberate, effective giving.",
     keywords: "raise, charity, join raise, amf, against malaria foundation",
-    siteUrl: "https://joinraise.org/",
+    siteUrl: "https://" + env.CUSTOM_DOMAIN + "/",
     author: {
       name: "Raise",
-      url: "https://joinraise.org/",
+      url: "https://" + env.CUSTOM_DOMAIN + "/",
     },
   },
   plugins: [
     {
       resolve: "gatsby-plugin-canonical-urls",
       options: {
-        siteUrl: "https://joinraise.org/",
+        siteUrl: "https://" + env.CUSTOM_DOMAIN + "/",
       },
     },
     "gatsby-plugin-typescript",
