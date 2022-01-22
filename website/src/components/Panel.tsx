b05c54f010062ca7c484e5af19a6d272eb65d9d0
Adam Jones <domdomegg+git@gmail.com>
2022-01-22 16:45:51 +0000

    Update inconsistent PNGs to SVGs

--- website/src/components/Panel.tsx
+++ website/src/components/Panel.tsx
@@ -2,7 +2,7 @@ import classNames from "classnames"
 import * as React from "react"
 
 interface Props {
-  image?: string,
+  image?: string | React.ReactNode,
   title: string,
   text: React.ReactNode,
   className?: string,
@@ -12,7 +12,7 @@ const Panel: React.FC<Props> = ({
   image, title, text, className,
 }) => (
   <div className={classNames("shadow-raise rounded px-8 py-8 flex flex-col items-center", className)}>
-    {image && <img alt="" src={image} style={{ height: 64 }} className="mb-4" />}
+    {typeof image === "string" ? <img alt="" src={image} className="mb-4 h-16" /> : image}
     <h3 className="text-5xl font-normal pb-2">{title}</h3>
     <p className="text-xl font-light">{text}</p>
   </div>
