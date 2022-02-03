e38dcd7b6ff38d53661599f30a0e29d6d0cc0ff6
Malena Schmidt <43754196+Malena205@users.noreply.github.com>
2022-02-04 00:13:31 +0100

    Add Oviya

--- server/src/helpers/groups.ts
+++ server/src/helpers/groups.ts
@@ -54,6 +54,7 @@ const USER_MAP: Record<Env["STAGE"], Record<string, Group[]>> = {
     "9d5eef6abcc85deeb6db5a5ecde8a39f6a804272": [g.Cambridge], // Rahul Shah
     "4423c1c1d952c230b42d1b621935e4222bae2a56": [g.Cambridge], // Riya Mody
     "20e6081c59abea2fbca6c58b44c785ab7a80ca4f": [g.Oxford], // Neha Banerjee
+    "0361cc51f2c7f88fee8249b70da9c723bb7ee51d": [g.Oxford], // Oviya Anand
   },
 }
 
