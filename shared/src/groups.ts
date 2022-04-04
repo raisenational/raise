e4db2963ed16622733111a61798e3e1addc3e53f
Adam Jones <domdomegg+git@gmail.com>
2022-04-04 23:37:11 +0100

    Remove Imperial pages (#149)

--- shared/src/groups.ts
+++ shared/src/groups.ts
@@ -13,7 +13,6 @@ export const groups = [
   "Oxford",
   "Sheffield",
   "Warwick",
-  "Imperial",
 
   "Demo",
   "Test",
--- website/src/pages/privacy.tsx
+++ website/src/pages/privacy.tsx
@@ -21,7 +21,7 @@ const PrivacyPage = () => (
         { text: "Oxford", href: "/oxford/" },
         { text: "Sheffield", href: "/sheffield/" },
         { text: "Warwick", href: "/warwick/" },
-        { text: "Imperial", href: "/imperial/" },
+        { text: "Alumni", href: "/alumni/" },
       ]}
       right={[
       ]}
