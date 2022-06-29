241dc7a133455c1c8d2a3a15e6dba3edab80e66b
Adam Jones <domdomegg+git@gmail.com>
2022-06-29 22:14:34 +0100

    Homepage: add concept summary video (#171)

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -5,7 +5,7 @@ import { convert } from "@raise/shared"
 import Page from "../components/Page"
 import Section, { SectionTitle } from "../components/Section"
 import Navigation from "../components/Navigation"
-import { CTADown } from "../components/Cover"
+import Cover, { CTADown } from "../components/Cover"
 import IntroStats from "../components/IntroStats"
 import PhilosophyV2 from "../components/PhilosophyV2"
 import ContactForm from "../components/ContactForm"
@@ -20,36 +20,39 @@ const IndexPage = () => (
       <title>Raise</title>
       <meta property="og:title" content="Raise" />
     </Helmet>
-    <Navigation
-      left={[
-        { text: "Cambridge", href: "https://www.mayweekalternative.org.uk/" },
-        { text: "Durham", href: "/durham/" },
-        { text: "Glasgow", href: "/glasgow/" },
-        { text: "Oxford", href: "/oxford/" },
-        { text: "Sheffield", href: "/sheffield/" },
-        { text: "Warwick", href: "/warwick/" },
-        { text: "Alumni", href: "/alumni/" },
-      ]}
-      right={[
-      ]}
-    />
-    <Section className="px-8 my-16">
-      <IntroStats
-        title="Raise: A Celebration of Giving"
-        tagline="Raise is a charitable movement encouraging students to adopt a positive approach towards deliberate, effective giving. Check out our chapter websites to learn more about what we do."
-        statistics={{
-          years: 4 + 1,
-          students: 1366 + 733,
-          raised: 284581 + 100445,
-          protected: 340013 + convert.moneyToPeopleProtected("gbp", 10044544),
-        }}
+    <Cover>
+      <Navigation
+        left={[
+          { text: "Cambridge", href: "https://www.mayweekalternative.org.uk/" },
+          { text: "Durham", href: "/durham/" },
+          { text: "Glasgow", href: "/glasgow/" },
+          { text: "Oxford", href: "/oxford/" },
+          { text: "Sheffield", href: "/sheffield/" },
+          { text: "Warwick", href: "/warwick/" },
+          { text: "Alumni", href: "/alumni/" },
+        ]}
+        right={[
+        ]}
       />
+      <Section className="px-8 my-16">
+        <IntroStats
+          title="Raise: A Celebration of Giving"
+          tagline="Raise is a charitable movement encouraging students to adopt a positive approach towards deliberate, effective giving. Check out our chapter websites to learn more about what we do."
+          statistics={{
+            years: 4 + 1,
+            students: 1366 + 733,
+            raised: 284581 + 100445,
+            protected: 340013 + convert.moneyToPeopleProtected("gbp", 10044544),
+          }}
+        />
 
-    </Section>
-    <CTADown
-      text="Our philosophy"
-      href="#our-philosophy"
-    />
+        <iframe className="w-full max-w-2xl mx-auto mt-12 rounded shadow-raise" title="YouTube: Raise: A Celebration of Giving" width="672" height="378" src="https://www.youtube-nocookie.com/embed/6bEloAzIV2c?rel=0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+      </Section>
+      <CTADown
+        text="Our philosophy"
+        href="#our-philosophy"
+      />
+    </Cover>
 
     <PhilosophyV2 />
 
