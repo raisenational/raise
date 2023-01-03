6ca94c31fa0f868853da77fd758805a4f777cd6d
Adam Jones <domdomegg+git@gmail.com>
2023-01-03 20:54:31 +0000

    Add login loading states

---
+++ web/src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+
+const Spinner: React.FC = () => (
+  <div className="border-t-transparent animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status" aria-label="Loading" />
+)
+
+export default Spinner
