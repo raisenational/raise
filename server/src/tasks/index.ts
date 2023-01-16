71200719a589c7ebbd1cc8c2ddd58e84c860afda
Adam Jones <domdomegg+git@gmail.com>
2023-01-16 21:11:53 +0000

    Add group initialisation task

--- server/src/tasks/index.ts
+++ server/src/tasks/index.ts
@@ -1,10 +1,12 @@
 import { TaskDefinition } from "../helpers/types"
 import sendSlackTestMessage from "./sendSlackTestMessage"
 import logStackTrace from "./logStackTrace"
+import intialiseGroups from "./intialiseGroups"
 
 const tasks: TaskDefinition[] = [
   sendSlackTestMessage,
   logStackTrace,
+  intialiseGroups,
 ]
 
 export default tasks
--- shared/src/groups.ts
+++ shared/src/groups.ts
@@ -3,6 +3,7 @@ const mapOf = <T extends string>(keys: readonly T[]): { [K in T]: K } => keys.re
   return acc
 }, Object.create(null))
 
+/** @deprecated */
 export const groups = [
   "National",
   "NationalTech",
@@ -21,6 +22,13 @@ export const groups = [
   "Test",
 ] as const
 
+/** @deprecated */
 export const g = mapOf(groups)
 
+/** @deprecated */
 export type Group = keyof typeof g;
+
+export const fixedGroups = {
+  National: "01GPY6D06N6EHEE0RGRNZJ60K7",
+  NationalTech: "01GPY6D06NTBC0674WHR7H60YT",
+}
