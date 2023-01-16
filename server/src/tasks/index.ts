e2ab6d022c8b29d89d69580a93426a518ffa7fab
Adam Jones <domdomegg+git@gmail.com>
2023-01-16 22:03:22 +0000

    Fix typo

--- server/src/tasks/index.ts
+++ server/src/tasks/index.ts
@@ -1,12 +1,12 @@
 import { TaskDefinition } from "../helpers/types"
 import sendSlackTestMessage from "./sendSlackTestMessage"
 import logStackTrace from "./logStackTrace"
-import intialiseGroups from "./intialiseGroups"
+import initializeGroups from "./initializeGroups"
 
 const tasks: TaskDefinition[] = [
   sendSlackTestMessage,
   logStackTrace,
-  intialiseGroups,
+  initializeGroups,
 ]
 
 export default tasks
--- server/src/tasks/initializeGroups.ts
+++ server/src/tasks/initializeGroups.ts
@@ -5,7 +5,7 @@ import { groupTable } from "../helpers/tables"
 // Purpose: For creating National and NationalTech groups with consistent IDs
 export default {
   id: "01GPY6CSHMD09C16H417C8SVBB",
-  name: "Initialise National and NationalTech groups",
+  name: "Initialize National and NationalTech groups",
   run: async (): Promise<void> => {
     await insert(groupTable, { id: fixedGroups.National, name: "National" })
     await insert(groupTable, { id: fixedGroups.NationalTech, name: "NationalTech" })
