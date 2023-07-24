a01bd5d7f54f78a10086d3a14cbceab35c42d4eb
Deevan Parmar <Deevan.parmar@outlook.com>
2023-07-24 19:11:13 +0100

    Add clearer Donations Warnings
    People were not seeing these warnings, so we made it clearer by making it coloured and animated

--- apps/web/src/components/DonationPage.tsx
+++ apps/web/src/components/DonationPage.tsx
@@ -439,6 +439,7 @@ const DonationFormDonate: React.FC<{ formMethods: UseFormReturn<DonationFormResp
     donationAmount = parseMoney(watches.donationAmount);
   } catch { /* noop */ }
   const shouldShowLowAmountWarning = donationAmount !== null && ((watches.recurrenceFrequency === 'ONE_OFF' && donationAmount <= 15_00) || (watches.recurrenceFrequency === 'WEEKLY' && donationAmount < 2_00));
+  const shouldShowHighAmountWarning = donationAmount !== null && ((watches.recurrenceFrequency === 'ONE_OFF' && donationAmount >= 2000_00) || (watches.recurrenceFrequency === 'WEEKLY' && donationAmount > 200_00));
   const schedule = donationAmount === null ? null : calcPaymentSchedule(donationAmount, 0, watches.recurrenceFrequency === 'ONE_OFF' ? null : watches.recurrenceFrequency, fundraiser.recurringDonationsTo);
   const totalDonationAmount = schedule === null ? null : schedule.now.donationAmount + schedule.future.reduce((acc, cur) => acc + cur.donationAmount, 0);
   const matchFundingAmount = totalDonationAmount === null ? null : calcMatchFunding({
@@ -530,7 +531,8 @@ const DonationFormDonate: React.FC<{ formMethods: UseFormReturn<DonationFormResp
         })}
       />
 
-      {touchedFields.donationAmount && shouldShowLowAmountWarning && <p className="mt-1">We encourage all students to donate an amount that's personally significant to them - and we know this is different for everyone! A good way of thinking about is to donate an amount that really makes you think about where and why you are donating. We can't wait to celebrate with you soon!</p>}
+      {touchedFields.donationAmount && shouldShowLowAmountWarning && <p className="my-3 py-2 px-3 font-normal animate-shake text-[#a66a02] border border-[#a66a02] border-1 rounded-lg bg-[#ffec41]">We encourage all students to donate an amount that's personally significant to them - and we know this is different for everyone! A good way of thinking about is to donate an amount that really makes you think about where and why you are donating. We can't wait to celebrate with you soon!</p>}
+      {touchedFields.donationAmount && shouldShowHighAmountWarning && <p className="my-3 py-2 px-3 font-normal animate-shake text-red-800 border border-red-800 border-1 rounded-lg bg-red-50">(Placeholder for High Donations Message)</p>}
 
       {fundraiser.matchFundingRate !== 0 && fundraiser.matchFundingPerDonationLimit !== null && fundraiser.matchFundingRemaining !== 0 && (
       <p className="mt-1">
@@ -675,7 +677,7 @@ const DonationFormCelebrate: React.FC<{ formMethods: UseFormReturn<DonationFormR
             })}
           />
 
-          {touchedFields.contributionAmount && contributionAmount > 20_00 && <p className="mt-1">While you're welcome to contribute as much as you want, we don't expect to spend more than £20 per head on the Summer Party.</p>}
+          {touchedFields.contributionAmount && contributionAmount > 20_00 && <p className="my-3 py-1 px-2 font-normal animate-shake text-[#a66a02] border border-[#a66a02] border-1 rounded-lg bg-[#ffec41]">While you're welcome to contribute as much as you want, we don't expect to spend more than £20 per head on the Summer Party.</p>}
         </>
       )}
 
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,51 +1,62 @@
-const defaultTheme = require("tailwindcss/defaultTheme")
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       colors: {
-        "raise-purple": "#5D215E",
-        "raise-purple-light": "#6D376E",
-        "raise-purple-dark": "#4A1A4B",
+        'raise-purple': '#5D215E',
+        'raise-purple-light': '#6D376E',
+        'raise-purple-dark': '#4A1A4B',
 
-        "raise-red": "#AD2E53",
-        "raise-red-light": "#B54264",
-        "raise-red-dark": "#8A2442",
+        'raise-red': '#AD2E53',
+        'raise-red-light': '#B54264',
+        'raise-red-dark': '#8A2442',
 
-        "raise-blue": "#2ECAD6",
-        "raise-blue-light": "#42CFDA",
-        "raise-blue-dark": "#24A1AB",
+        'raise-blue': '#2ECAD6',
+        'raise-blue-light': '#42CFDA',
+        'raise-blue-dark': '#24A1AB',
 
-        "raise-orange": "#E88452",
-        "raise-orange-light": "#EA9063",
-        "raise-orange-dark": "#B96941",
+        'raise-orange': '#E88452',
+        'raise-orange-light': '#EA9063',
+        'raise-orange-dark': '#B96941',
 
-        "raise-yellow": "#F2CA1A",
-        "raise-yellow-light": "#F3CF30",
-        "raise-yellow-dark": "#C1A114",
+        'raise-yellow': '#F2CA1A',
+        'raise-yellow-light': '#F3CF30',
+        'raise-yellow-dark': '#C1A114',
       },
       fontFamily: {
-        "raise-header": ["laca", ...defaultTheme.fontFamily.sans],
-        "raise-content": ["mr-eaves-sans", ...defaultTheme.fontFamily.sans],
+        'raise-header': ['laca', ...defaultTheme.fontFamily.sans],
+        'raise-content': ['mr-eaves-sans', ...defaultTheme.fontFamily.sans],
       },
       skew: {
-        15: "15deg",
-        "-15": "-15deg",
+        15: '15deg',
+        '-15': '-15deg',
       },
       boxShadow: {
-        raise: "0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)",
+        raise: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
       },
       keyframes: {
         breathe: {
           '0%, 100%': { transform: 'scale(1.05)' },
           '50%': { transform: 'scale(1)' },
         },
+        wave: {
+          '0%': { transform: 'rotate(0.0deg)' },
+          '10%': { transform: 'rotate(0.7deg)' },
+          '20%': { transform: 'rotate(-1.0deg)' },
+          '30%': { transform: 'rotate(0.7deg)' },
+          '40%': { transform: 'rotate(-1.0deg)' },
+          '50%': { transform: 'rotate(0.7deg)' },
+          '60%': { transform: 'rotate(0.0deg)' },
+          '100%': { transform: 'rotate(0.0deg)' },
+        },
       },
       animation: {
         breathe: 'breathe 2s ease-in-out infinite',
+        shake: 'wave 2s linear',
       },
     },
   },
   plugins: [],
-}
+};
