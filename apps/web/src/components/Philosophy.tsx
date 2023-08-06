f2133dd46f931484b8ae7ff9363ef757816ff9ce
Adam Jones <domdomegg+git@gmail.com>
2023-08-06 18:39:03 +0100

    Fix Philosophy section spacing (#305)

--- apps/web/src/components/Philosophy.tsx
+++ apps/web/src/components/Philosophy.tsx
@@ -94,21 +94,12 @@ const Philosophy: React.FC<PhilosophyProps> = ({ brand = 'Raise' }) => {
             <CakeIcon width={60} height={60} className="block mx-auto my-4 hover:scale-105 active:scale-0 transition-all" />
             <p className="my-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200">But it's much rarer that we stop and think about giving. Most of these opportunities either hide the act of giving from view - so we don't even notice it's happening -  or push us to give only when we're directly asked.</p>
             <p className="my-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200">
-              At
-              {brand}
-              , we think that's a
-              {' '}
-              <span className="font-bold">real missed opportunity</span>
-              .
+              At {brand}, we think that's a <span className="font-bold">real missed opportunity</span>.
             </p>
             <p className="my-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200">We believe that when we pause to think about our giving, the experience can be incredibly rewarding.</p>
             <p className="my-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200">When we properly consider why we are giving and the impact our money will have - in short, when we give deliberately - we can really appreciate how positive donating to charity can be.</p>
             <p className="my-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200">
-              This way, giving becomes more meaningful, enjoyable, sustainable and ultimately more
-              <span className="whitespace-nowrap">
-                <span className="animate-breathe inline-block cursor-pointer" onClick={launchConfetti} onKeyDown={(e) => { if (e.key === 'Enter' || e.key === ' ') { launchConfetti(); e.preventDefault(); } }} role="button" tabIndex={0}>impactful</span>
-                .
-              </span>
+              This way, giving becomes more meaningful, enjoyable, sustainable and ultimately more <span className="whitespace-nowrap"><span className="animate-breathe inline-block cursor-pointer" onClick={launchConfetti} onKeyDown={(e) => { if (e.key === 'Enter' || e.key === ' ') { launchConfetti(); e.preventDefault(); } }} role="button" tabIndex={0}>impactful</span>.</span>
             </p>
             <div data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200"><HeartIcon width={60} height={60} className="block mx-auto my-4 hover:scale-125 active:scale-95 transition-all cursor-pointer" onClick={launchConfetti} /></div>
           </div>
@@ -119,25 +110,14 @@ const Philosophy: React.FC<PhilosophyProps> = ({ brand = 'Raise' }) => {
         <Section className="skew-y-6 -mt-1">
           <div className="max-w-2xl mx-auto text-left -mt-8">
             <h2 className="mt-20 mb-4 text-2xl text-center sm:text-4xl font-raise-header font-black" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200">
-              How does
-              {' '}
-              {brand}
-              {' '}
-              work?
+              How does {brand} work?
             </h2>
             <p className="my-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200">
-              We invite students to join us and celebrate the end of the academic year by making a personally significant donation to charity - an amount that will
-              {' '}
-              <span className="font-bold">make you think about what and why you are giving</span>
-              .
+              We invite students to join us and celebrate the end of the academic year by making a personally significant donation to charity - an amount that will <span className="font-bold">make you think about what and why you are giving</span>.
             </p>
 
             <Quote className="my-12" by="Chloe Shieh, Donor" headshotSrc={chloeShieh} imagePlacement="right" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200">
-              It took me a long time to decide to join
-              {' '}
-              {brand}
-              {' '}
-              - I'd never given that much before - but when I finally donated, I saw our incredible impact and it really made donating an amazing positive experience.
+              It took me a long time to decide to join {brand} - I'd never given that much before - but when I finally donated, I saw our incredible impact and it really made donating an amazing positive experience.
             </Quote>
 
             <p className="my-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200">Then, at the end of the academic year, we come together as a community for our Summer Party. It's a time when we can reflect on and celebrate our impact, while enjoying the end of the year with a wonderful group of people.</p>
@@ -166,55 +146,25 @@ const Philosophy: React.FC<PhilosophyProps> = ({ brand = 'Raise' }) => {
           <div className="max-w-2xl mx-auto text-left -mt-8">
             <h2 className="mt-20 mb-4 text-2xl text-center sm:text-4xl font-raise-header font-black" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200">Our vision</h2>
             <p className="my-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200">
-              Our hope is that through making a
-              {' '}
-              <span className="font-bold">personally significant donation</span>
-              {' '}
-              and then coming together to celebrate that, you'll be able to see the
-              {' '}
-              <span className="font-bold">huge impact</span>
-              {' '}
-              giving effectively can have and come to feel as positive about it as we do.
+              Our hope is that through making a <span className="font-bold">personally significant donation</span> and then coming together to celebrate that, you'll be able to see the <span className="font-bold">huge impact</span> giving effectively can have and come to feel as positive about it as we do.
             </p>
             <p className="my-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200">
-              Ultimately,
-              {' '}
-              <span className="font-bold">we want to change the culture around charity</span>
-              . We want charity to be something that is actively embraced and enjoyed, and a meaningful part of all of our lives.
+              Ultimately, <span className="font-bold">we want to change the culture around charity</span>. We want charity to be something that is actively embraced and enjoyed, and a meaningful part of all of our lives.
             </p>
 
             <Quote className="my-12" by="Jake Mendel, Donor" headshotSrc={jakeMendel} imagePlacement="right" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200">
-              {brand}
-              {' '}
-              really changed the way I approach giving. It's made me want to make deliberate giving a permanent part of my life even now I've graduated so, after joining
-              {' '}
-              {brand}
-              , I decided to donate 10% of my income to effective charities every year.
+              {brand} really changed the way I approach giving. It's made me want to make deliberate giving a permanent part of my life even now I've graduated so, after joining {brand}, I decided to donate 10% of my income to effective charities every year.
               <br />
               <br />
-              I never really used to think about giving to charity, but I loved joining
-              {' '}
-              {brand}
-              {' '}
-              and I now can't wait to donate again in the future.
+              I never really used to think about giving to charity, but I loved joining {brand} and I now can't wait to donate again in the future.
             </Quote>
 
             <p className="my-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200">
-              So, however you've chosen to give before, why not join
-              {' '}
-              {brand}
-              {' '}
-              this year and see how
-              <span className="font-bold">charity can become a meaningful and positive part of all of our lives</span>
-              .
+              So, however you've chosen to give before, why not join {brand} this year and see how <span className="font-bold">charity can become a meaningful and positive part of all of our lives</span>.
             </p>
 
             <p className="mt-16 mb-4" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200">
-              P.S. Want to get to know us more? Check out testimonies from
-              {' '}
-              <Link href="/yearbook" className="text-blue-300 hover:text-blue-400">our community yearbook</Link>
-              {' '}
-              or watch our founder's TEDx Talk, 'Rethinking Celebration: The Positive Case for Giving', which talks all about our feel-good philosophy!
+              P.S. Want to get to know us more? Check out testimonies from <Link href="/yearbook" className="text-blue-300 hover:text-blue-400">our community yearbook</Link> or watch our founder's TEDx Talk, 'Rethinking Celebration: The Positive Case for Giving', which talks all about our feel-good philosophy!
             </p>
             <iframe className="w-full rounded shadow-raise" title="YouTube: Rethinking Celebration: The Positive Case for Giving" width="672" height="378" src="https://www.youtube-nocookie.com/embed/fvjeG7xE-wE?rel=0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-offset="200" />
           </div>
