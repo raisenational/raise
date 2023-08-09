0f12cb850ee803a517c0ecf52811e72b575b9e0e
Deevan Parmar <Deevan.parmar@outlook.com>
2023-08-09 23:21:05 +0100

    Removed unnecessary Comments

--- apps/server/src/api/admin/tasks/get.ts
+++ apps/server/src/api/admin/tasks/get.ts
@@ -1,14 +1,6 @@
-/* import { fixedGroups } from '@raise/shared'; */
 import { middyfy } from '../../../helpers/wrapper';
 import { $Tasks, Task } from '../../../schemas';
 import tasks from '../../../tasks';
-/* import { assertHasGroup } from '../../../helpers/db'; */
-
-/* export const main = middyfy(null, $Tasks, true, async (event) => {
-  assertHasGroup(event, fixedGroups.National);
-  return (tasks.map((t) => ({ id: t.id, name: t.name })));
-});
-*/
 
 export const main = middyfy(null, $Tasks, true, async (event) => {
   const overlap = (a: string[], b: string[]): boolean => a.some((v) => b.includes(v));
