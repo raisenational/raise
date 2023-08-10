511ead5ad616c883a214a2def1aecc919dc32e58
Deevan Parmar <Deevan.parmar@outlook.com>
2023-08-10 01:31:19 +0100

    Moved sendtestUserEmail to another sub-branch

--- apps/server/src/tasks/index.ts
+++ apps/server/src/tasks/index.ts
@@ -3,14 +3,12 @@ import sendSlackTestMessage from './sendSlackTestMessage';
 import logStackTrace from './logStackTrace';
 import initializeGroups from './initializeGroups';
 import sendTestConfirmationEmail from './sendTestConfirmationEmail';
-import sendTestUserEmail from './sendTestUserEmail';
 
 const tasks: TaskDefinition[] = [
   sendSlackTestMessage,
   logStackTrace,
   initializeGroups,
   sendTestConfirmationEmail,
-  sendTestUserEmail,
 ];
 
 export default tasks;
