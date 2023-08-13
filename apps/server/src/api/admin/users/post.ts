d695d6dfc82baa3bd4e93f3f106e90877210bc44
DeevanP <97373812+DeevanP@users.noreply.github.com>
2023-08-13 23:29:02 +0100

    Improve user creation performance by moving scan into conditional (#315)

--- apps/server/src/api/admin/users/post.ts
+++ apps/server/src/api/admin/users/post.ts
@@ -14,11 +14,11 @@ export const main = middyfy($UserCreation, $Ulid, true, async (event) => {
     ...event.body,
   });
 
-  // finds name of the logged in user who made the user. If for some reason, the user's name is undefined, it will use The Raise National Team instead
-  const usersFromDb = await scan(userTable);
-  const sender: string = usersFromDb.find((u) => u.email === event.auth.payload.subject)?.name ?? 'The Raise National Team';
-
   if (event.body.sendAccountCreationEmail === true) {
+    // finds name of the logged in user who made the user. If for some reason, the user's name is undefined, it will use The Raise National Team instead
+    const usersFromDb = await scan(userTable);
+    const sender: string = usersFromDb.find((u) => u.email === event.auth.payload.subject)?.name ?? 'The Raise National Team';
+
     await sendEmail(
       'Your account has been created!',
       newUser(event.body, sender),
