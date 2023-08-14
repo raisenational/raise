698d796be4e7828b48d2e66d02b538da60a074f5
Deevan Parmar <Deevan.parmar@outlook.com>
2023-08-14 01:56:52 +0100

    Tidy get Task

--- apps/server/src/api/admin/tasks/get.ts
+++ apps/server/src/api/admin/tasks/get.ts
@@ -1,15 +1,10 @@
 import { middyfy } from '../../../helpers/wrapper';
-import { $Tasks, Task } from '../../../schemas';
+import { $Tasks } from '../../../schemas';
 import tasks from '../../../tasks';
 
 export const main = middyfy(null, $Tasks, true, async (event) => {
   const overlap = (a: string[], b: string[]): boolean => a.some((v) => b.includes(v));
   const listOfAllTasks = tasks.map((t) => ({ id: t.id, name: t.name, groups: t.groups }));
-  const listOfTasks: Task[] = [];
-  listOfAllTasks.forEach((task) => {
-    if (overlap(event.auth.payload.groups, task.groups)) {
-      listOfTasks.push(task);
-    }
-  });
-  return (listOfTasks.map((t) => ({ id: t.id, name: t.name })));
+
+  return (listOfAllTasks.filter((task) => overlap(event.auth.payload.groups, task.groups)));
 });
