63e12e724e0fae1cd0e224a5a069ec9b6f34e9ae
DeevanP <97373812+DeevanP@users.noreply.github.com>
2023-08-20 04:19:01 +0100

    Add user creator as sender for new user notification emails (#317)

--- apps/server/src/api/admin/users/post.ts
+++ apps/server/src/api/admin/users/post.ts
@@ -17,13 +17,13 @@ export const main = middyfy($UserCreation, $Ulid, true, async (event) => {
   if (event.body.sendAccountCreationEmail === true) {
     // finds name of the logged in user who made the user. If for some reason, the user's name is undefined, it will use The Raise National Team instead
     const usersFromDb = await scan(userTable);
-    const sender: string = usersFromDb.find((u) => u.email === event.auth.payload.subject)?.name ?? 'The Raise National Team';
+    const sender: string = usersFromDb.find((u) => u.email === event.auth.payload.subject)?.name.concat(' (via Raise National)') ?? 'The Raise National Team';
 
     await sendEmail(
       'Your account has been created!',
       newUser(event.body, sender),
       event.body.email,
-      'Raise National',
+      sender,
     );
   }
 
