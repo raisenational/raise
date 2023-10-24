dc14787d585b9a85df51c498f8b5ad5a6b9720ce
Deevan Parmar <Deevan.parmar@outlook.com>
2023-10-24 01:37:34 +0100

    Re-subscription to campaign

--- apps/server/src/api/public/members/campaign/post.ts
+++ apps/server/src/api/public/members/campaign/post.ts
@@ -1,7 +1,7 @@
 import { ulid } from 'ulid';
 import createHttpError from 'http-errors';
 import { middyfy } from '../../../../helpers/wrapper';
-import { insert, scan } from '../../../../helpers/db';
+import { insert, scan, update } from '../../../../helpers/db';
 import { $CampaignMemberCreation, $Ulid } from '../../../../schemas';
 import { campaignMemberTable, memberTable } from '../../../../helpers/tables';
 
@@ -9,15 +9,20 @@ export const main = middyfy($CampaignMemberCreation, $Ulid, false, async (event)
   if ((event.body.name === '') || (event.body.name === undefined)) throw new createHttpError.BadRequest('No Name Provided');
   if ((event.body.email === '') || (event.body.email === undefined)) throw new createHttpError.BadRequest('No Email Provided');
   if (event.body.email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) === (false || null)) throw new createHttpError.BadRequest('Invalid Email Format');
-  // This is a general intrest list
+
   const usersFromDb = await scan(memberTable);
   const campaignMembersFromDb = await scan(campaignMemberTable);
 
   let member = usersFromDb.find((u) => u.email === event.body.email);
 
   if (member) {
-    if (campaignMembersFromDb.find((cm) => (cm.memberId === member?.id) && (cm.campaignId === event.body.campaignId))) {
+    if (campaignMembersFromDb.find((cm) => (cm.memberId === member?.id) && (cm.campaignId === event.body.campaignId) && (cm.active === true))) {
       throw new createHttpError.BadRequest('This email has already been used to sign up to this campaign');
+    } else if (campaignMembersFromDb.find((cm) => (cm.memberId === member?.id) && (cm.campaignId === event.body.campaignId) && (cm.active === false))) {
+      await update(campaignMemberTable, { memberId: member.id, campaignId: event.body.campaignId }, { active: true }); // re-signs-up member to the campaign (if they had previously unsubscribed)
+      if (member.name !== event.body.name) {
+        await update(memberTable, { id: member.id }, { name: event.body.name }); // updates the name should there be a change in name
+      }
     }
     await insert(campaignMemberTable, {
       id: ulid(),
@@ -40,19 +45,6 @@ export const main = middyfy($CampaignMemberCreation, $Ulid, false, async (event)
       active: true,
     });
   }
-
-  // (done) day/timestamp of sign up
-  // joinraise.org/bristol/signup?utm_campaign=Freshers fair 2023
-  // (done) unsubscribe page, enter email (Perhaps autofill joinraise.org/unsubscribe?email=adam@joinraise.org)
-  // (done) unsubscribe property for members
-  // auto clear form when success, maybe clear success message after a few seconds
-  // string array for chapters
-  // mini-segments within chapters???
-  // joinraise.org./signup/{campaign} (Exmaple RSVP form)
-  // sign up to Mailchimp and what kind of list (subset of a table) and campaigns.
-  // Subscribers Table (name, email, day of signup, opt out, id), kinda what the member list is right now
-  // then another list table for each chapter, (Summer party, Raise bristol 2022, Raise Bristol 2023),
-  // then another list that say which person is on which list.
 
   return member.id;
 });
