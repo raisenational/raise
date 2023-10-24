4b5fe12e44d8f8d06258dc33393e984b6419bb2b
Deevan Parmar <Deevan.parmar@outlook.com>
2023-10-24 04:53:46 +0100

    Add export feature for campaign members

--- apps/web/src/pages/admin/campaign.tsx
+++ apps/web/src/pages/admin/campaign.tsx
@@ -1,7 +1,8 @@
 import { RouteComponentProps } from '@gatsbyjs/reach-router';
 import { useState } from 'react';
-import { PlusSmIcon } from '@heroicons/react/outline';
+import { DownloadIcon, PlusSmIcon } from '@heroicons/react/outline';
 import { fixedGroups, format } from '@raise/shared';
+import jsonexport from 'jsonexport/dist';
 import { asResponseValues, useRawReq, useReq } from '../../helpers/networking';
 import Section, { SectionTitle } from '../../components/Section';
 import Table from '../../components/Table';
@@ -12,6 +13,7 @@ import Modal from '../../components/Modal';
 import { Form } from '../../components/Form';
 import { EmailCreation } from '../../helpers/generated-api-client';
 import ErrorPage from '../../components/ErrorPage';
+import Link from '../../components/Link';
 
 const CampaignPage: React.FC<RouteComponentProps & { campaignId: string }> = ({ campaignId }) => {
   const [campaigns] = useReq('get /admin/campaigns');
@@ -20,8 +22,12 @@ const CampaignPage: React.FC<RouteComponentProps & { campaignId: string }> = ({ 
   const campaignMembers = asResponseValues(allMembers.data?.filter((u) => u.campaignId === campaignId), allMembers);
   const [members] = useReq('get /public/members');
 
+  const marketingEmailsMembers = asResponseValues(members.data?.filter((m) => campaignMembers.data?.find((cm) => (cm.memberId === m.id) && (cm.active === true) && (cm.emailConsent === true))), members); // members of this campaign that have given consent to recieving emails
+  const allEmailMembers = asResponseValues(members.data?.filter((m) => campaignMembers.data?.find((cm) => (cm.memberId === m.id) && (cm.active === true))), members); // all members of this campaign, regardless of consent
+
   const [newEmailsModalOpen, setNewEmailsModalOpen] = useState(false);
   const [viewEmailModalOpen, setViewEmailModalOpen] = useState(false);
+  const [exportModalOpen, setExportModalOpen] = useState(false);
   const [viewEmailSubject] = useState('');
   const [viewEmailMessage] = useState('');
   const [emails, refetchEmails] = useReq('get /admin/campaigns/emails');
@@ -49,6 +55,33 @@ const CampaignPage: React.FC<RouteComponentProps & { campaignId: string }> = ({ 
     );
   }
 
+  const downloadFn = (data: object[] | undefined, name: string) => (data ? async () => {
+    const csv = await jsonexport(data);
+    if (csv) {
+      const encodedUri = encodeURI(`data:text/csv;charset=utf-8,${csv}`);
+      const link = document.createElement('a');
+      link.setAttribute('href', encodedUri);
+      link.setAttribute('download', `${(`${name}_at_${new Date().toISOString().slice(0, 10)}`).replace(/[^a-z0-9]+/gi, '_').toLowerCase()}.csv`);
+      document.body.appendChild(link);
+      link.click();
+    }
+  } : undefined);
+
+  const downloadAllEmails = downloadFn(
+    allEmailMembers.data
+      ?.map((m) => ({
+        name: m.name, email: m.email, 'NOTE some members may have NOT given consent to receive emails': '',
+      })),
+    `${campaign.data.chapter}_${campaign.data.campaign}_emails`,
+  );
+
+  const downloadMarketingEmails = downloadFn(
+    marketingEmailsMembers.data
+      ?.map((m) => ({
+        name: m.name, email: m.email, 'Remember to use mail merge or BCC!': '',
+      })),
+    `${campaign.data.chapter}_${campaign.data.campaign}_marketing_emails`,
+  );
   // to do list
   // create preview button
   // allow for users to see a copy of how the email looks
@@ -116,7 +149,33 @@ const CampaignPage: React.FC<RouteComponentProps & { campaignId: string }> = ({ 
         />
       </div>
       <div>
-        <SectionTitle> Members </SectionTitle>
+        <div className="flex">
+          <SectionTitle className="flex-1"> Members </SectionTitle>
+          <Button onClick={() => setExportModalOpen(true)}>
+            <DownloadIcon className="h-6 mb-1" />
+            {' '}
+            Export
+          </Button>
+        </div>
+        <Modal open={exportModalOpen} onClose={() => setExportModalOpen(false)}>
+          <SectionTitle>Export Data</SectionTitle>
+          <p className="my-4">By downloading this data you agree to use it only for the stated purpose, and will handle it securely and in line with the security policy. All exports will be in CSV format, which can be used in spreadsheeting tools such as Excel, LibreOffice Calc or Google Sheets.</p>
+
+          <h2 className="mt-8 text-2xl">I want to send marketing emails to donors</h2>
+          <p className="my-2">This contains donor names and emails who have agreed to receive marketing emails from Raise. You must not share this data with other organisations. Ideally use a mail merge tool or proper email software (such as Mailchimp) to send emails. If not, make sure you BCC recipients.</p>
+          <Button onClick={downloadMarketingEmails} variant="blue">Download names and emails</Button>
+
+          <h2 className="mt-8 text-2xl">I want to send non-marketing emails to donors</h2>
+          <p className="my-2">
+            This contains all donor names and emails. Some of these may not have agreed to receive marketing emails. Make sure any emails you send do not contain marketing messages, for example promoting a certain thing such as taking a pledge. It is generally okay to use this list to send summer party RSVP or feedback surveys. More guidance on what is and isn't marketing is available from
+            <Link href="https://ico.org.uk/media/for-organisations/documents/1555/direct-marketing-guidance.pdf#page=17"> the ICO</Link>
+            {' '}
+            or the national team. You must not share this data with other organisations. Ideally use a mail merge tool or proper email software (such as Mailchimp) to send emails. If not, make sure you BCC recipients.
+          </p>
+          <Button onClick={downloadAllEmails} variant="blue">Download names and emails</Button>
+
+          <p className="mt-8">Got another use case? Contact the national team with feedback on what data you'd like to get.</p>
+        </Modal>
         <Table
           className="mb-8"
           definition={{
