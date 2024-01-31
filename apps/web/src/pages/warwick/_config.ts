d68fd6b66930bab35b1c1ebb8d8a0f471331b12a
Adam Jones <domdomegg+git@gmail.com>
2024-01-31 23:50:22 +0000

    Link 2024 prod fundraisers (#373)

--- apps/web/src/pages/warwick/_config.ts
+++ apps/web/src/pages/warwick/_config.ts
@@ -4,7 +4,7 @@ const config: ChapterConfig = {
   fundraiserIds: {
     local: '01FGNSHH6X6X878ZNBZKY44JQA', // Raise Demo
     dev: '01FTPS7SHMFVBPZP9Z5G2W82G9',
-    prod: '01GNSQV8K2MVW4EKM5BQDEVTJP',
+    prod: '01HNGXKKDV6X683MWJZY9BV8PE',
   },
   title: 'Raise Warwick',
   brand: 'Raise',
