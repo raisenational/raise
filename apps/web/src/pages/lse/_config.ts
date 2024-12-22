b071bb927053e608e552fd7587fb454d2aeabf19
Adam Jones <domdomegg+git@gmail.com>
2024-12-22 13:05:20 +0000

    web: Add Raise LSE page

---
+++ apps/web/src/pages/lse/_config.ts
@@ -0,0 +1,13 @@
+import { ChapterConfig } from '../../helpers/types';
+
+const config: ChapterConfig = {
+  fundraiserIds: {
+    local: '01FGNSHH6X6X878ZNBZKY44JQA', // Raise Demo
+    dev: '',
+    prod: '', // This will need to be updated with the actual LSE fundraiser ID when available
+  },
+  title: 'Raise LSE',
+  brand: 'Raise',
+};
+
+export default config;
---
+++ apps/web/src/pages/lse/donate.tsx
@@ -0,0 +1,8 @@
+import DonationPage from '../../components/DonationPage';
+import config from './_config';
+
+const Page = () => (
+  <DonationPage {...config} />
+);
+
+export default Page;
