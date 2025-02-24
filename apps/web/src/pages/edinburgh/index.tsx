87003856e752aceac10f216a33a1352e09b0f660
victoriabrach11 <20vab02@gmail.com>
2025-02-24 19:59:29 +0000

    Update index.tsx

--- apps/web/src/pages/edinburgh/index.tsx
+++ apps/web/src/pages/edinburgh/index.tsx
@@ -45,6 +45,7 @@ const IndexPage = () => (
 						protected: convert.moneyToPeopleProtected('gbp', 18658_00),
 					}}
 				/>
+				<Button variant="outline" size="large" className="mt-4 mb-12" href="donate/">Donate</Button>
 			</Section>
 			<CTADown text='How it works' href='#how-it-works' />
 		</Cover>
