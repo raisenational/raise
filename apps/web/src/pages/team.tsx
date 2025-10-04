292fcdfba0aca88101e57e42ca323e86d3faacd3
adam jones <domdomegg+git@gmail.com>
2025-10-04 15:45:20 +0100

    Update national team for 2025 (#422)
    - Replace old team members with 2025 roster (kept Adam Jones)
    - Update all bios with current roles and information
    - Replace headshots with square-cropped images for proper display
    - Update Adam's bio with current Anthropic role
    - Update Adam's headshot from adamjones.me
    - Remove unused team member images

--- apps/web/src/pages/team.tsx
+++ apps/web/src/pages/team.tsx
@@ -6,58 +6,46 @@ import Quote from '../components/Quote';
 
 const NATIONAL_TEAM = [
 	{
-		name: 'Olivia Collotta',
-		role: 'Director',
-		img: '/shared/images/olivia.jpg',
-		bio: 'Olivia is a final year student at the University of Edinburgh studying Politics. Since co-founding Raise Edinburgh in 2022, she has served as a mentor to other Raise chapters and lead the community team. She is incredibly excited to be helping take Raise forward into another year of meaningful student engagement and celebration! Outside of her more fruitful academic pursuits, she enjoys trying to keep up with current events and trying to adopt her neighborhood\'s stray cats.',
-	},
-	{
 		name: 'Ella Denton',
-		role: 'Community lead',
+		role: 'Director',
 		img: '/shared/images/ella.jpg',
-		bio: 'Ella is a final year History student at UCL. She founded Raise UCL, London\'s first Raise chapter, as a first year in 2022.\nThis year, she will be taking on the role of Community Lead - organising the mentor program as well as mentoring several chapters herself. She is really excited about growing Raise at other London universities, with LSE among the chapters she will be mentoring.\nOutside of Raise and uni, Ella writes films, works as a part-time nanny to fund her penchant for pretty dresses, and then sleeps because all that\'s pretty exhausting. Top tip: when you meet her, time how long it takes her to mention that she used to live in Sweden.',
+		bio: 'Ella will be directing this year\'s National Team while pursuing a part-time Master\'s in Public History at UCL. She founded Raise UCL, London\'s first Raise chapter, in 2022, moving on to the role of Community Lead and mentor last year. This makes her position as Director all the more exciting; she is thrilled to be taking Raise forward, prioritising Rep engagement and ensuring that the Raise delivers advice that is applicable to chapters at all different kinds of university, from collegiate to large cities.\n\nIn her spare time, Ella writes films, works as a nanny and improves her near-terrifying level of sitcom knowledge.',
 	},
 	{
 		name: 'Maxime Alves',
-		role: 'UCL mentor & Advisory board member',
+		role: 'Head of Advisory Board',
 		img: '/shared/images/maxime.jpg',
-		bio: 'Maxime is studying Management Science at UCL, and was involved in establishing Raise\'s first London-based chapter as its Secretary-Treasurer. He will be joining the Advisory Board working on bettering Raise processes and fostering a sense of community while also mentoring newer chapters.\n\nIn his spare time, Maxime enjoys cooking, going to the movies and spontaneously trying new things.',
+		bio: 'Maxime is currently pursuing his Master\'s at Imperial College London. He first helped establish Raise\'s first London-based chapter, where he served as Secretary-Treasurer. After joining the Advisory Board, he has now stepped into the role of Head, where he focuses on strengthening Raise\'s processes, fostering a strong sense of community, and ensuring the charity continues to grow and stay relevant.\n\nOutside of Raise, Maxime enjoys cooking, going to the movies, and discovering new experiences.',
 	},
 	{
-		name: 'Shahamath Meehal Hussain',
-		role: 'Warwick mentor',
-		img: '/shared/images/meehal.jpg',
-		bio: 'Meehal was the Founding President of Raise Warwick, where he worked to set up the chapter for two years. Leaving it in capable hands, he joined the national team to continue mentoring Warwick and other chapters. Meehal really enjoyed his time leading Raise Warwick, where he grew to understand and embody the values and core mission of Raise. He found it rewarding to inspire others in the same cause. He continues to donate to AMF and other such charities and encourages his fellow graduates to do so, too.\n\nMeehal now works at Lloyds Banking Group on their Finance Graduate Scheme, where his current role empowers him to hold companies accountable against LBG\'s ESG mission statements and continues to get involved in fundraising and volunteering initiatives. In his free time Meehal enjoys playing piano, tutoring maths and learning Japanese in preparation for moving there one day.',
+		name: 'Princess Bisnar',
+		role: 'Expansion and Founding Officer',
+		img: '/shared/images/princess.jpg',
+		bio: 'Princess is this year\'s Expansion and Founding Officer for the Raise National Team. She just completed her BSc in Psychology at UCL and is now going into an MPH at Imperial. Having been co-president for Raise UCL last year, she believes that Raise is a great community because of our passion for changing our way of giving so that it is more memorable and rewarding. She can\'t wait to see Raise expand with more branches around the country!',
 	},
 	{
-		name: 'Varshneyee Dutt',
-		role: 'Advisory board chair',
-		img: '/shared/images/varshneyee.jpg',
-		bio: 'Varshneyee is a final year student of English Language & Linguistics at the University of Glasgow. She has been part of Raise since last year, when she was President of the Glasgow chapter, and is thrilled to join the national team this year.\n\nIn her free time, she enjoys reading Victorian literature, watching period dramas and travelling.',
+		name: 'Talia',
+		role: 'Community Lead',
+		img: '/shared/images/talia.jpg',
+		bio: 'Talia is a fourth year Psychology student at UCL and is the community lead for 25/26. She was the Co-President of Raise last year and loved getting involved in an initiative that has a unique approach to giving, as well as getting to know people who were also passionate about Raise and having an impact. She is very excited to continue this further this year and helping Raise feel more even more connected.\n\nOutside of Raise, Talia is a massive cat-person and is currently fostering cats and helping them adjust to a home environment!',
 	},
 	{
-		name: 'Louis Danker',
-		role: 'Expansion & founding',
-		img: '/shared/images/louis.jpg',
-		bio: 'Louis is focussed on expanding Raise to more universities, and helping found new chapters. He was Co-Founding President of Raise Edinburgh in 2022, and is keen to keep spreading the celebration of giving far and wide! Louis is a final year Geography student in Edinburgh. He\'s a big football fan (and terrible footballer), a big music fan (and terrible musician), and a big foodie (his cooking divides opinion).',
+		name: 'Aarushi',
+		role: 'Advisory Board member and mentor',
+		img: '/shared/images/aarushi.jpg',
+		bio: 'Aarushi is a 4th year medical student at Cambridge, and was co-president of MWA in 2024-25. This year she will be joining the national team as a mentor for Cambridge as well as joining the Advisory Board and coming up with ways to make Raise bigger and better. In her spare time, Aarushi enjoys playing any racket sport, eating her housemate\'s nice cooking and watching House MD as a form of revision.',
 	},
 	{
-		name: 'Bill Chen',
-		role: 'Advisory board member',
-		img: '/shared/images/bill.jpg',
-		bio: 'Bill is on the advisory board of the Raise National Team. Before this role, he led the University of Oxford Raise chapter while studying Philosophy, Politics and Economics. During this time, Bill revitalised the inactive chapter, culminating in its most successful year.\n\nOn the advisory board, he aims to find solutions to make Raise chapters more resilient in the longer term. Bill is excited by the potential of cross-chapter collaboration to improve Raise\'s agility.',
+		name: 'Chiara',
+		role: 'Advisory Board member and mentor',
+		img: '/shared/images/chiara.jpg',
+		bio: 'Chiara is a passionate philosophy student at the University of Edinburgh and is currently on her year abroad in Leuven, Belgium.\nThis year she will mentor Raise Edinburgh as well as being part of the advisory board to make very exciting projects come true!\nRaise for her has been an opportunity to make a tangible change in the world and she is hyped to continue being part of the movement.\nIn her free time she loves hiking, petting every animal she can (especially cows), never shutting up about her dog and bothering people with fun facts!',
 	},
 	{
 		name: 'Adam Jones',
 		role: 'Technology & governance',
-		bio: 'Adam is responsible for technology at Raise. He maintains the website and our open-source donations platform. He also manages our relationship with the Charity Commission, and serves as a trustee on our board.\n\nIn his day job, he works as a software engineer helping people to have more impact in their careers. Outside work, he enjoys cycling around London, contributing to open-source projects and playing board games.',
+		bio: 'Adam is responsible for technology at Raise. He maintains the website and our open-source donations platform. He also manages our relationship with the Charity Commission, and serves as a trustee on our board.\n\nIn his day job, he is a member of technical staff at Anthropic, working to ensure the responsible development of advanced AI systems. Outside work, he enjoys cycling around London, contributing to open-source projects and playing board games.',
 		img: '/shared/images/adam.jpg',
-	},
-	{
-		name: 'Thomas Cohen',
-		role: 'Treasurer',
-		bio: 'Thomas has done a number of different jobs on the Raise National Team, including managing the accounts and mentoring. He was heavily involved in Raise in Durham, acting as Vice President and President over its first two years. He is really passionate about positive and celebratory giving, and therefore has pledged to give 10% of his income to charity!\n\nThomas studied History at Durham, and last year completed a master\'s in History and Philosophy of Science at UCL.',
-		img: '/shared/images/thomas.jpg',
 	},
 ];
 
@@ -84,7 +72,7 @@ const TeamPage = () => (
 
 			<p className='font-black font-raise-header mt-8 mb-2'>National Team</p>
 			<p>
-				There's
+				There are
 				{' '}
 				{NATIONAL_TEAM.length}
 				{' '}
